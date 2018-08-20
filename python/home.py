0578d8fe728c6267cfb443cb63197607a3035c4d
adarsh-ms <adarshms1997@outlook.com>
2018-08-20 21:36:23 +0530

    feat : Add behaviour for welcome menu & user input

--- python/home.py
+++ python/home.py
@@ -14,3 +14,27 @@ class mainMenu(object):
         elif platform == "win32":
             
             self.clear = lambda: os.system('cls')
+        
+        
+        self.welcomeScreen()
+    
+    
+    def welcomeScreen(self):
+        
+        self.clear() # clear console
+        
+        print("\n\n\n\t Welcome to banking services....... ") # welcome message
+        time.sleep(2) # sleep for 2 sec
+        
+        self.clear() # clear console
+        
+        ##Menu 
+        
+        print(''' \n\n\t  MENU 
+                  
+                  1. Sign Up (New Customer)
+                  2. Sign In (Existing Customer)
+                  3. Admin Sign In
+                  4. Quit ''')
+
+        self.choice = input('\n Enter your choice : ') # To capture desired input
