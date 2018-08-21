6aa8d3fc1deb2d5af5971530334d7d50778f81dd
adarsh-ms <adarshms1997@gmail.com>
2018-08-21 11:14:09 +0530

    refactor : Add signUp menu items in a single class

--- python/home.py
+++ python/home.py
@@ -1,22 +1,80 @@
 import os, time
 from sys import platform
 
+
+##To clear console according to the platform 
+        
+if platform == "linux" or platform == "linux2" or platform == "darwin":
+ 
+    clear = lambda: os.system('clear')
+     
+elif platform == "win32":
+     
+    clear = lambda: os.system('cls')
+            
+
 class mainMenu(object):
     
     def __init__(self):
         
-        ##To clear console according to the platform 
-        
-        if platform == "linux" or platform == "linux2" or platform == "darwin":
-        
-            self.clear = lambda: os.system('clear')
-            
-        elif platform == "win32":
-            
-            self.clear = lambda: os.system('cls')
-        
         
         self.welcomeScreen()  # To display main menu at start
+    
+    
+    def subMenu(self): # Function to handle user input
+        
+        if self.choice == 1 :   # sign in
+            
+            signUpMenu_Object = signUpMenu()
+            signUpMenu_Object.signUp()
+        
+        elif self.choice == 2 : # sign out
+            
+            self.signIn()
+        
+        elif self.choice == 3 : # admin sign in
+            
+            self.adminSignIn()
+
+        elif self.choice == 4 : # quit
+            
+            self.quit()
+        
+        else :                 # Invalid choice 
+            
+            print("Sorry! System can't determine the request....")
+            self.welcomeScreen()
+    
+    
+    def welcomeScreen(self): # Function to display main menu & prompting user choice
+        
+        clear() # clear console
+        
+        print("\n\n\n\t Welcome to banking services....... ") # welcome message
+        time.sleep(2) # sleep for 2 sec
+        
+        clear() # clear console
+        
+        ##Menu 
+        
+        print(''' \n\n\t  MENU 
+                  
+                  1. Sign Up (New Customer)
+                  2. Sign In (Existing Customer)
+                  3. Admin Sign In
+                  4. Quit ''')
+
+        self.choice = int(input('\n Enter your choice : ')) # To capture desired input
+    
+        self.subMenu() # To interpret user choice & perform specific functions
+
+
+class signUpMenu(object) :
+    
+    
+    def __init__(self):
+        
+        self.desposit = 0    # Initializing of deposit money
     
     
     def userAddress(self):
@@ -50,30 +108,34 @@ class mainMenu(object):
                     
                     self.desposit = int(input('\n Enter the amount to deposit : Rs. '))
                 
-                else :
-                    
-                    self.deposit = 0
-                
+        
             elif self.accntType == 'c' or self.accntType == 'C' :   # Prompt if current account
                 
-                    decision = input("\n Note : 'Current account' need a minimum balance of Rs. 5000. \n Do you wish to continue ? [y/n]")
-                    if decision == 'y' or decision == 'Y' : # If choice is to make the payment
-                        
-                        self.desposit = int(input('\n Enter the amount to deposit : Rs. '))    
+                decision = input("\n Note : 'Current account' need a minimum balance of Rs. 5000. \n Do you wish to continue ? [y/n]")
+                if decision == 'y' or decision == 'Y' : # If choice is to make the payment
                     
-                    else :  # Prompt for changing the account type or cancel application
+                    self.desposit = int(input('\n Enter the amount to deposit : Rs. '))    
+                
+                else :  # Prompt for changing the account type or cancel application
+                    
+                    decision = input("\n Do you wish to change account type or cancel the process ? [y/q]")
+                    if decision == 'y' or decision == 'Y' :
                         
-                        decision = input("\n Do you wish to change account type or cancel the process ? [y/q]")
-                        if decision == 'y' or decision == 'Y' :
-                            
-                            self.accountType()
+                        self.accountType()
+                    
+                    else : # If choice is to cancel application
                         
-                        else : # If choice is to cancel application
-                            
-                            print("\n Processing your request... Please wait.....")
-                            time.sleep(1)
-                            self.welcomeScreen()
-    
+                        print("\n Processing your request... Please wait.....")
+                        time.sleep(1)
+                        self.welcomeScreen()
+            
+            else : 
+                
+                print("\n Sorry! Our bank doesn't provide this type of account. Please choose from the available options.....")
+                time.sleep(2)
+                self.accountType()
+                
+            
     
     def signUp(self): # Function for new user registration data collection
         
@@ -82,53 +144,18 @@ class mainMenu(object):
         self.userName()
         self.userAddress()
         self.accountType()
-                    
         
-    def subMenu(self): # Function to handle user input
         
-        if self.choice == 1 :   # sign in
+        decision = input('\n Your account is to be created. Do you wish to proceed or quit ? [y/q]')
+        
+        if decision == 'y' or decision == 'Y' :
             
-            self.signUp()
-        
-        elif self.choice == 2 : # sign out
+            clear()
+            print("\n Processing your request..... Please wait.....")
+            time.sleep(2)
             
-            self.signIn()
-        
-        elif self.choice == 3 : # admin sign in
-            
-            self.adminSignIn()
-
-        elif self.choice == 4 : # quit
-            
-            self.quit()
-        
-        else :                 # Invalid choice 
-            
-            print("Sorry! System can't determine the request....")
-            self.welcomeScreen()
-    
-    
-    def welcomeScreen(self): # Function to display main menu & prompting user choice
-        
-        self.clear() # clear console
-        
-        print("\n\n\n\t Welcome to banking services....... ") # welcome message
-        time.sleep(2) # sleep for 2 sec
-        
-        self.clear() # clear console
-        
-        ##Menu 
-        
-        print(''' \n\n\t  MENU 
-                  
-                  1. Sign Up (New Customer)
-                  2. Sign In (Existing Customer)
-                  3. Admin Sign In
-                  4. Quit ''')
-
-        self.choice = int(input('\n Enter your choice : ')) # To capture desired input
-    
-        self.subMenu() # To interpret user choice & perform specific functions
+            clear()
+            print("\n Creating user account...... This may take a moment......")
 
                             
             
